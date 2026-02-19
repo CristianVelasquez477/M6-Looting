84ac0f80db3da0a8e4d54e398de7202df83f0ed6
CristianVelasquez477 <crvelazquez@s.sfusd.edu>
2026-02-19 13:32:18 -0800

    M6-L5

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,6 +1,6 @@
 extends Area2D
 @onready var canvas_group: CanvasGroup = $CanvasGroup
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 func _ready() -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", 4.0)
 	
@@ -12,3 +12,20 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", 4.0)
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
+	var event_is_mouse_click:bool = (
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+		
+
+	)
+	
+	if event_is_mouse_click:
+		open()
+		
+func open() -> void:
+	animation_player.play("open")
+
+	input_pickable = false
